36e990dd989ebb637cab8ebc78b3f60ac0d5f3d4
Sockbee <102143186+Sockbee@users.noreply.github.com>
2024-09-18 06:09:44 +0000

    공백 수정

--- .ccls-cache/@home@runner@replitalgorithm/algorithm@greedy@3_숫자카드게임.cpp
+++ .ccls-cache/@home@runner@replitalgorithm/algorithm@greedy@3_숫자카드게임.cpp
@@ -3,14 +3,15 @@
 #include <vector>
 using namespace std;
 
-/*int main()
+int exec()
 {
   int N, M;
   cin >> N >> M;
 
-  vector <vector<int>> v(M, vector<int>(N, 0)); // M개의 행, N개의 열로 된 2차원
-vector 배열 생성하며 0으로 초기화 for(int i = 0; i < N; i++){ for(int j = 0; j <
-M; j++){ cin >> v[i][j];
+  vector <vector<int>> v(M, vector<int>(N, 0)); // M개의 행, N개의 열로 된 2차원vector 배열 생성하며 0으로 초기화 
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j <M; j++){
+      cin >> v[i][j];
     }
   }
   vector<int> min;
@@ -22,4 +23,4 @@ M; j++){ cin >> v[i][j];
     max = *max_element(min.begin(), min.end());
   }
   cout << max;
-}*/
+}
