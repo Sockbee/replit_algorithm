06ee11d0e9a238a548530c6c3ea787880c4b9d1c
Sockbee <102143186+Sockbee@users.noreply.github.com>
2024-09-18 04:42:11 +0000

    cache

---
+++ .ccls-cache/@home@runner@replitalgorithm/algorithm@greedy@main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int N, M, K;
+
+  cin >> N >> M >> K;
+  vector<int> arr;
+  for(int i = 0; i < N; i++){
+    int num;
+    cin >> num;
+    arr.push_back(num);
+  }
+
+  sort(arr.begin(), arr.end(), greater<int>());
+
+  int ans = 0;
+  int a = M / (K + 1);
+  ans += (arr[0] * K + arr[1]) * a + arr[0] * (M % (K + 1));
+
+  printf("%d\n", ans);
+}
