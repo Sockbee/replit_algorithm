d8bd3d29bb9967a32283e6090f57421d8feef581
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-05-06 13:07:10 +0000

    10773 해결

--- algorithm/solved_ac/10773 제로.py
+++ algorithm/solved_ac/10773 제로.py
@@ -2,3 +2,15 @@ import sys
 input = sys.stdin.readline
 print = sys.stdout.write
 
+K = int(input().rstrip())
+
+stack = []
+for _ in range(K):
+    n = int(input().rstrip())
+    if (n == 0):
+        stack.pop()
+    else:
+        stack.append(n)
+
+ans = sum(stack)
+print(str(ans))
