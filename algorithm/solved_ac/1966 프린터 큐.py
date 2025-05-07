5e2d287d2adad6b540897027a728a5d1bfb5675d
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-05-07 13:28:30 +0000

    1966 해결

--- algorithm/solved_ac/1966 프린터 큐.py
+++ algorithm/solved_ac/1966 프린터 큐.py
@@ -1,8 +1,9 @@
 import sys
 input = sys.stdin.readline
-#print = sys.stdout.write
+print = sys.stdout.write
 
 T = int(input().rstrip())
+ans = []
 for _ in range(T):
     N, M = list(map(int, input().rstrip().split()))
     lst = []
@@ -10,16 +11,22 @@ for _ in range(T):
     for i in range(N):
         lst.append([i, nums[i]])
 
-    for i in range(N):
-        flag = 0
-        for j in range(1, N):
-            if (lst[i][1] < lst[j][1]):
-                flag = 1
-                num = lst.pop(i)
-                lst.append(num)
-            print(lst)
-        print(lst)
+    for k in range(N):
+        for i in range(N):
+            flag = 0
+            for j in range(k + 1, N):
+                if (lst[k][1] < lst[j][1]):
+                    n = lst.pop(k)
+                    lst.append(n)
+                    flag = 1
+                    break
+            if (flag == 0): break
+
                 
     for i in range(N):
         if (M == lst[i][0]):
-            print(str(i + 1))
+            ans.append(i + 1)
+            break
+
+for i in range(T):
+    print(str(ans[i]) + '\n')
