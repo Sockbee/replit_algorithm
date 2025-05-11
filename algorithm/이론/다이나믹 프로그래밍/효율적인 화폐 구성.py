b246031db5e80fda88ea37ad8e9fe3c6153b59af
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-05-11 09:46:00 +0000

    이코테 예제 5번 해결

--- algorithm/이론/다이나믹 프로그래밍/효율적인 화폐 구성.py
+++ algorithm/이론/다이나믹 프로그래밍/효율적인 화폐 구성.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 N, M = list(map(int, input().rstrip().split()))
 
 arr_money = []
-d = [10001] * (M + 1)
+d = [10001] * 10001
 for _ in range(N):
     a = int(input().rstrip())
     arr_money.append(a)
@@ -17,7 +17,8 @@ for money in arr_money:
 #for i in range(6, n + 1):
 #    d[i] = min(d[i-3], d[i-5]) + 1
 max_money = arr_money[-1]
-for i in range()
+for i in range(max_money + 1, M + 1):
+    d[i] = min(d[i - money] for money in arr_money) + 1
 
 if (d[M] >= 10000):
     print(-1)
