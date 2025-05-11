8dd658370d2746dc886703b5dac536bda40e21e3
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-05-11 06:58:12 +0000

    효 화 푸는중

---
+++ algorithm/이론/다이나믹 프로그래밍/효율적인 화폐 구성.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N, M = list(map(int, input().rstrip().split()))
+
+arr_money = []
+d = [10001] * (M + 1)
+for _ in range(N):
+    a = int(input().rstrip())
+    arr_money.append(a)
+
+#d[3] = d[5] = 1
+for money in arr_money:
+    d[money] = 1
+
+
+#for i in range(6, n + 1):
+#    d[i] = min(d[i-3], d[i-5]) + 1
+max_money = arr_money[-1]
+for i in range()
+
+if (d[M] >= 10000):
+    print(-1)
+else:
+    print(d[M])
