4ea2cb0b037586c58b96eb1917eb382ae2095b90
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-06-01 05:37:48 +0000

    10026 해결

--- algorithm/solved_ac/CLASS 3/10026 적록색약.py
+++ algorithm/solved_ac/CLASS 3/10026 적록색약.py
@@ -37,4 +37,35 @@ while pairs:
                 pairs.remove((ny, nx))
                 queue.append((ny, nx))
 
+print(cnt, end = ' ')
+
+visited = [[False] * n for _ in range(n)]
+pairs = { (i, j) for i in range(n) for j in range(n) }
+
+cnt = 0
+while pairs:
+    cnt += 1
+    py, px = pairs.pop()
+    visited[py][px] = True
+    queue.append((py, px))
+    current = graph[py][px]
+    
+    while queue:
+        y, x = queue.popleft()
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 > nx or nx >= n or 0 > ny or ny >= n:
+                continue
+            if visited[ny][nx] == False:
+                if graph[y][x] != 'B' and graph[ny][nx] != 'B':
+                    visited[ny][nx] = True
+                    pairs.remove((ny, nx))
+                    queue.append((ny, nx))
+                elif graph[y][x] == 'B' and graph[ny][nx] == 'B':
+                    visited[ny][nx] = True
+                    pairs.remove((ny, nx))
+                    queue.append((ny, nx))
 print(cnt)
