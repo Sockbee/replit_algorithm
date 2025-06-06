5dadcd9311a24afe0f6fa4ca08ca58080a269ac3
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-06-06 08:00:01 +0000

    15654 해결

--- algorithm/solved_ac/CLASS 4/ESSENSIAL/15654 N과 M (5).py
+++ algorithm/solved_ac/CLASS 4/ESSENSIAL/15654 N과 M (5).py
@@ -1,11 +1,13 @@
 import sys
-from itertools import combinations
+from itertools import permutations
 input = sys.stdin.readline
 
 N, M = map(int, input().rstrip().split()) #1부터 N까지 자연수에 M개의 수
 
-nums = range(1, N + 1)
-combi = list(combinations(nums, M))
+nums = map(int, input().rstrip().split())
+combi = list(permutations(nums, M))
+
+combi.sort()
 
 for tup in combi:
     for i in range(M):
