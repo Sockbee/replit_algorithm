f0515ece7430256744c1883ddda7086e80c01c3b
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-06-22 04:21:33 +0000

    파이썬 포매팅 사용

--- algorithm/solved_ac/CLASS 3/11724 연결 요소의 개수.py
+++ algorithm/solved_ac/CLASS 3/11724 연결 요소의 개수.py
@@ -1,20 +1,22 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
 visited = set()
 
+
 def bfs(graph, start):
-  queue = deque([start])
-  visited.add(start)
+    queue = deque([start])
+    visited.add(start)
 
-  while queue:
-    node = queue.popleft()
+    while queue:
+        node = queue.popleft()
 
-    for neighbor in graph[node]:
-      if neighbor not in visited:
-        visited.add(neighbor)
-        queue.append(neighbor)
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
 
 
 N, M = list(map(int, input().rstrip().split()))
@@ -23,14 +25,14 @@ graph1 = [[] for _ in range(0, N + 1)]
 all_node = deque(range(1, N + 1))
 
 for i in range(M):
-  a, b = list(map(int, input().rstrip().split()))
-  graph1[a].append(b)
-  graph1[b].append(a)
+    a, b = list(map(int, input().rstrip().split()))
+    graph1[a].append(b)
+    graph1[b].append(a)
 
 cnt = 0
 while all_node:
-  bfs(graph1, all_node[0])
-  cnt += 1
-  all_node = deque(set(all_node) - visited)
+    bfs(graph1, all_node[0])
+    cnt += 1
+    all_node = deque(set(all_node) - visited)
 
 print(cnt)
