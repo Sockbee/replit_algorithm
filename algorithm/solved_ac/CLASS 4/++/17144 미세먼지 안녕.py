246a8bebb6d328be5cdb0cc5e887fe3285ddd695
Sockbee <102143186+Sockbee@users.noreply.github.com>
2025-07-24 14:58:26 +0000

    17144 해결

--- algorithm/solved_ac/CLASS 4/++/17144 미세먼지 안녕.py
+++ algorithm/solved_ac/CLASS 4/++/17144 미세먼지 안녕.py
@@ -1,15 +1,11 @@
 import sys
 input = sys.stdin.readline
 
-R, C, T = map(int, input().split()) # R행 C열 구하는 T초 시간 후
+R, C, T = map(int, input().split())  # R행 C열, T초 후
 
-graph = [[] for _ in range(R)]
+graph = [list(map(int, input().split())) for _ in range(R)]
 
-for i in range(R):
-    lst = list(map(int, input().split()))
-    graph[i] = lst
-
-#공기청정기 머리 위치 기록
+# 공기청정기 위치
 purifier = 0
 for i in range(R):
     if graph[i][0] == -1:
@@ -21,26 +17,57 @@ dy = [0, 0, -1, 1]
 
 time = 0
 while time < T:
-    #1. 미세먼지가 확산된다.
+    time += 1
+
+    # 1. 미세먼지 확산
+    temp = [[0] * C for _ in range(R)]
     for i in range(R):
         for j in range(C):
             if graph[i][j] > 0:
-                kes = []
-                for k in range(4):#4방향 탐색
-                    ni = i + dx[k]
-                    nj = j + dy[k]
-                    if 0 > ni or ni >= R or 0 > nj or nj >= C:
-                        continue
-                    if graph[ni][nj] != -1:
-                        kes.append(k)
-                added_value = graph[i][j] // 5
-                graph[i][j] -= added_value * len(kes)
-                for k in kes:
-                    ni = i + dx[k]
-                    nj = j + dy[k]
-                    graph[ni][nj] += added_value
+                spread_amount = graph[i][j] // 5
+                count = 0
+                for d in range(4):
+                    ni = i + dx[d]
+                    nj = j + dy[d]
+                    if 0 <= ni < R and 0 <= nj < C and graph[ni][nj] != -1:
+                        temp[ni][nj] += spread_amount
+                        count += 1
+                temp[i][j] -= spread_amount * count
 
-    #2. 공기청정기가 작동한다.
-    #공기청정기는 가장 윗행, 아랫행과 두 칸 이상 떨어져 있다.
-                    
-            
+    for i in range(R):
+        for j in range(C):
+            if graph[i][j] != -1:
+                graph[i][j] += temp[i][j]
+
+    # 2. 공기청정기 작동
+
+    # 위쪽: 반시계 방향
+    for i in range(purifier - 1, 0, -1):
+        graph[i][0] = graph[i - 1][0]
+    for j in range(C - 1):
+        graph[0][j] = graph[0][j + 1]
+    for i in range(purifier):
+        graph[i][C - 1] = graph[i + 1][C - 1]
+    for j in range(C - 1, 1, -1):
+        graph[purifier][j] = graph[purifier][j - 1]
+    graph[purifier][1] = 0
+
+    # 아래쪽: 시계 방향
+    for i in range(purifier + 2, R - 1):
+        graph[i][0] = graph[i + 1][0]
+    for j in range(C - 1):
+        graph[R - 1][j] = graph[R - 1][j + 1]
+    for i in range(R - 1, purifier + 1, -1):
+        graph[i][C - 1] = graph[i - 1][C - 1]
+    for j in range(C - 1, 1, -1):
+        graph[purifier + 1][j] = graph[purifier + 1][j - 1]
+    graph[purifier + 1][1] = 0
+
+# 최종 미세먼지 양 계산
+result = 0
+for i in range(R):
+    for j in range(C):
+        if graph[i][j] > 0:
+            result += graph[i][j]
+
+print(result)
